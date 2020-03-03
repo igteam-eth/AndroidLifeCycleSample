793c6fb6040c51447068855c222162c4a98ccc72
igteam-eth <igteam@114>
2020-03-03 14:40:35 +0700

    add Android Life Cycle console log

--- app/src/main/java/com/ethernom/helloworld/MainActivity.kt
+++ app/src/main/java/com/ethernom/helloworld/MainActivity.kt
@@ -11,14 +11,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        Log.e("LifeCycle", "OnCreate Called")
         var clickCount = 0
         button.setOnClickListener {
             clickCount++
             text_result.text  = "${text_result.text} \nhello world $clickCount"
         }
     }
+    override fun onStart() {
+        super.onStart()
+        Log.e("LifeCycle", "OnStart Called")
+    }
 
+    override fun onResume() {
+        super.onResume()
+        Log.e("LifeCycle", "OnResume Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("LifeCycle", "OnPause Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.e("LifeCycle", "OnRestart Called")
+    }
 
     override fun onStop() {
         super.onStop()
