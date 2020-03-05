1ac3e3d92543aa7e7fd27910d84daff4daa90650
igteam-eth <igteam@114>
2020-03-05 15:17:52 +0700

    - push notification to handle when callback from broadcast receiver

--- app/src/main/java/com/ethernom/helloworld/MyAppReceiver.java
+++ app/src/main/java/com/ethernom/helloworld/MyAppReceiver.java
@@ -1,28 +1,66 @@
 package com.ethernom.helloworld;
 
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.util.Log;
-
 import androidx.annotation.RequiresApi;
-
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+import static android.content.Context.NOTIFICATION_SERVICE;
 public class MyAppReceiver extends BroadcastReceiver {
-
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onReceive(Context context, Intent intent) {
-
         //Which App need to relaunch ?
         //What is mechanism to relaunch the App?
         Log.e("LifeCycle", "Need to relaunch App");
-        Intent intent1 = new Intent(context, MainActivity.class);
+        /*PackageManager pm = context.getPackageManager();
+        Intent launchIntent = pm.getLaunchIntentForPackage("com.ethernom.helloworld");
+        launchIntent.putExtra("some_data", "value");
+        context.startActivity(launchIntent);*/
+        silentNotification(context, NOTIFICATION_ID);
+    }
+    private static final String CHANNEL_ID = "alarm_channel";
+    private static final int NOTIFICATION_ID = 12345678;
+    @TargetApi(Build.VERSION_CODES.O)
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public static void createNotificationChannelSilent(Context ctx) {
+        final NotificationManager mgr = ctx.getSystemService(NotificationManager.class);
+        if(mgr == null) return;
+        final String name = ctx.getString(R.string.channel_name);
+        if(mgr.getNotificationChannel(name) == null) {
+            final NotificationChannel channel =
+                    new NotificationChannel(CHANNEL_ID, name, NotificationManager.IMPORTANCE_HIGH);
+            channel.setBypassDnd(true);
+            mgr.createNotificationChannel(channel);
+        }
+    }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void silentNotification(Context context, int mId) {
+        final int id = mId;
+        final NotificationManager manager =
+                (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+        createNotificationChannelSilent(context);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.ic_launcher_background);
+        builder.setColor(ContextCompat.getColor(context, R.color.colorAccent));
+        builder.setContentTitle(context.getString(R.string.app_name));
+        builder.setContentText("Click to relaunch hello world");
+        builder.setVisibility(NotificationCompat.VISIBILITY_SECRET);
+        builder.setAutoCancel(true);
+        builder.setPriority(Notification.PRIORITY_HIGH);
+        builder.setWhen(System.currentTimeMillis());
+        Intent intent = new Intent(context, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        context.startActivity(intent1);
-
-
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+        builder.setContentIntent(pendingIntent);
+        manager.notify(id, builder.build());
     }
 }
--- app/src/main/java/com/ethernom/helloworld/onClearFromRecentService.java
+++ app/src/main/java/com/ethernom/helloworld/onClearFromRecentService.java
@@ -5,30 +5,21 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 
-
 import android.os.IBinder;
-
 import android.util.Log;
 
-
-
 public class onClearFromRecentService extends Service {
-    String TAG = "onClearFromRecentService";
-
-
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
 
-
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d("ClearFromRecentService", "Service Started");
         return START_STICKY;
     }
-
 
     @Override
     public void onDestroy() {
@@ -42,10 +33,9 @@ public class onClearFromRecentService extends Service {
         AlarmManager am = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(this, MyAppReceiver.class);
         PendingIntent sender = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+ 10*1000, sender);
+        am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+ 5*1000, sender);
 
         Log.e("ClearFromRecentService", "App Killed");
-
 
     }
 
