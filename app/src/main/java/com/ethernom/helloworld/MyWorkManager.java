7d22fc02596aa072aea0b975818366c290748839
igteam-eth <igteam@114>
2020-04-20 10:50:19 +0700

    Base line - Helloworld with File Logging and Onetime WorkManager

---
+++ app/src/main/java/com/ethernom/helloworld/MyWorkManager.java
@@ -0,0 +1,28 @@
+package com.ethernom.helloworld;
+
+import android.content.Context;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+
+public class MyWorkManager extends Worker {
+
+    private Context mContext;
+    public MyWorkManager(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+        this.mContext = context;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @NonNull
+    @Override
+    public Result doWork() {
+
+        MyApplication.appendLog(MyApplication.getCurrentDate()+ " : MyWorkManager doWork \n");
+        return Result.success();
+    }
+}
